a4dfff0b628ffa703e96f0df2f4ab4e4d36daa6d
Leonardo Blautzik <leoblautzik@gmail.com>
2023-03-13 10:42:00 -0300

    clase 13/03/23

--- Repaso/src/repaso/EsPar.java
+++ Repaso/src/repaso/EsPar.java
@@ -1,16 +1,29 @@
 package repaso;
 
+import java.util.Scanner;
+
 public class EsPar {
 	
 	static boolean esPar(int n) {
-		
+		return n % 2 == 0;
 		
 	}
 
 	public static void main(String[] args) {
-		//leer un entero por teclado e informar si es par o impar
+		int numero;
+		Scanner miScanner = new Scanner(System.in);//leer un entero por teclado e informar si es par o impar
+		System.out.print("Ingrese un valor entero: ");
+		numero = miScanner.nextInt();
+		miScanner.close();
+		if (esPar(numero)) {
+			System.out.println(numero + " Es Par");
+		}
+			else {
+				System.out.println(numero + " Es Impar");
+			}
+		}
 		
 
-	}
+	
 
 }
